fa699c0296759dc175ca9445940abc8cd232145b
Deeksha Deepak <deekshad1502@gmail.com>
2025-12-31 12:02:10 +0530

    Testing sunbird collection editor

--- src/app/client/src/app/modules/player-helper/player-helper.module.ts
+++ src/app/client/src/app/modules/player-helper/player-helper.module.ts
@@ -18,7 +18,7 @@ import {QumlPlayerV2Service} from './service/quml-player-v2/quml-player-v2.servi
 // import { QumlPlayerService } from './service/quml-player/quml-player.service';
 import { QumlLibraryModule as QsLibModule, QuestionCursor as QsCursor} from '@project-sunbird/sunbird-quml-player';
 // import { QumlLibraryModule, QuestionCursor } from '@project-sunbird/sunbird-quml-player-v9';
-import { EditorCursor } from '@project-sunbird/sunbird-collection-editor';
+import { EditorCursor } from '@deeksha2309/sunbird-collection-editor';
 import { EditorCursor as QsEditorCursor } from '@project-sunbird/sunbird-questionset-editor';
 import { CslFrameworkService } from '../public/services/csl-framework/csl-framework.service';
 
